6e7d01f2ff678d8d9cb946fd83860ffe832ef01e
Manuel Schröder <manuel.schroeder@storyblok.com>
2025-08-08 18:36:40 +0200

    functional region/lang switcher including translatable slugs

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,94 +1,61 @@
 import { useStoryblokApi } from '@storyblok/astro';
 
 const getRegionCodes = () => {
+	// this could be managed in Storyblok as well
 	return ['us', 'eu'];
 };
 
-const languagesPerRegion = {
+const languageCodesPerRegion = {
 	// this could be managed in Storyblok as well
 	us: ['es'],
 	eu: ['es', 'fr', 'de'],
 };
 
 const getAvailableLanguagesForRegion = (region) => {
-	return languagesPerRegion[region] || [];
+	return languageCodesPerRegion[region] || [];
 };
 
-const getLanguageCodes = () => {
-	// disregard for now as await is not supported in Astro's JSX
-	/* const storyblokApi = useStoryblokApi();
+const getLanguageCodes = async () => {
+	const storyblokApi = useStoryblokApi();
 	const { data } = await storyblokApi.get('cdn/spaces/me');
 
-	return data.space.language_codes; */
-	return ['es', 'fr', 'de'];
+	return data.space.language_codes;
 };
 
-const getCleanSlug = (story) => {
-	let storySlug = story?.default_full_slug ?? story.full_slug;
-	const storySlugParts = storySlug.split('/');
+const getCleanSlug = async (slug) => {
+	const slugParts = slug.split('/');
 
-	const languages = getLanguageCodes();
+	const languages = await getLanguageCodes();
 	const regions = getRegionCodes();
 
-	if (languages.includes(storySlugParts[0])) {
+	if (languages.includes(slugParts[0])) {
 		// If the first part of the slug is a language code, remove it
-		storySlugParts.shift();
+		slugParts.shift();
 	}
 
-	if (regions.includes(storySlugParts[0])) {
+	if (regions.includes(slugParts[0])) {
 		// If the first part of the slug is a region code, remove it
-		storySlugParts.shift();
+		slugParts.shift();
 	}
 
-	return storySlugParts.join('/');
+	return slugParts.join('/');
 };
 
-const getI18nStorySlug = (currentLanguage, story, languages) => {
-	//console.log('currentLanguage', currentLanguage);
-	// this is strictly for translated slugs using the app
+const getTranslatedSlug = async (story, language) => {
+	if (!story.translated_slugs) return false;
+
 	const translatedSlug = story.translated_slugs?.find((slug) => {
-		return slug.path !== 'home' && slug.lang === currentLanguage;
+		return slug.path !== 'home' && slug.lang === language;
 	});
 
-	const defaultSlug = translatedSlug
-		? story.default_full_slug
-		: story.full_slug;
-
-	const cleanedSlug =
-		defaultSlug === 'home' || defaultSlug === 'eu/home' ? '' : defaultSlug;
-	const slug = translatedSlug?.path ?? cleanedSlug;
-
-	//console.log(slug, 'slug');
-
-	// If no current language is provided, return the slug without a preceding language code
-	// TODO: also use this function in the first part to account for default_full_slug not being there if no translated slug is set
-	if (!currentLanguage) {
-		const slugArray = slug.split('/');
-		//console.log('slugArray', slugArray);
-		// If the first part of the slug is a language code, remove it
-		if (languages.includes(slugArray[0])) slugArray.shift();
-		//console.log('slugArray after removing language code', slugArray.join('/'));
-		return slugArray.join('/');
-	}
-
-	// Match 'us/' at the start and remove it
-	if (/^us\//.test(slug)) {
-		return slug.replace(/^us\//, `${currentLanguage}/`);
-	}
-
-	// Match 'eu/' at the start and inject language after
-	if (/^eu\//.test(slug)) {
-		return slug.replace(/^eu\//, `eu/${currentLanguage}/`);
-	}
-
-	// If no region matched, return as-is or handle as needed
-	return slug;
+	if (!translatedSlug) return false;
+	return await getCleanSlug(translatedSlug.path);
 };
 
 export {
 	getLanguageCodes,
-	getI18nStorySlug,
-	getAvailableLanguagesForRegion,
 	getRegionCodes,
+	getAvailableLanguagesForRegion,
 	getCleanSlug,
+	getTranslatedSlug,
 };
