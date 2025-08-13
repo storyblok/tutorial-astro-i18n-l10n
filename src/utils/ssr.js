c34800e0c0830ee271c4d9f3fb9e59ae5114ac9c
Manuel Schröder <manuel.schroeder@storyblok.com>
2025-08-13 14:01:33 +0200

    updates

--- src/utils/ssr.js
+++ src/utils/ssr.js
@@ -1,5 +1,5 @@
 import { useStoryblokApi } from '@storyblok/astro';
-import { getLanguageCodes } from './i18n';
+import { getCleanSlug, getLanguageCodes } from './i18n';
 export async function getStoryblokPaths(region = 'us') {
 	const languages = await getLanguageCodes();
 	const storyblokApi = useStoryblokApi();
@@ -11,32 +11,35 @@ export async function getStoryblokPaths(region = 'us') {
 
 	const staticPaths = [];
 
-	for (const link of links) {
+	for await (const link of links) {
 		if (link.is_folder) continue;
-		const cleanedSlug = link.slug.slice(3); // Remove the region prefix (e.g., 'us/' or 'eu/') from the slug
+		const cleanSlug = await getCleanSlug(link.slug);
 
 		staticPaths.push({
 			params: {
-				slug: cleanedSlug === 'home' ? undefined : cleanedSlug,
-				// we probably also need to account for slugs like eu/es/home or es/home here.
-				// also for the region/language switcher
+				slug: cleanSlug.slug === 'home' ? undefined : cleanSlug.slug,
 			},
 		});
 
 		// These alternates are for field-level versions of the slug.
 		if (link.alternates && link.alternates.length > 0) {
-			for (const alternate of link.alternates) {
+			for await (const alternate of link.alternates) {
 				if (languages.includes(alternate.lang)) {
-					if (alternate.translated_slug === 'home') continue;
+					const cleanSlug = await getCleanSlug(alternate.translated_slug);
 					staticPaths.push({
 						params: {
-							slug: `${alternate.lang}/${alternate.translated_slug}`,
+							slug: `${alternate.lang}/${cleanSlug.slug === 'home' ? '' : cleanSlug.slug}`,
 						},
 					});
 				}
 			}
 		}
 	}
+
+	console.log(
+		`Generated ${staticPaths.length} static paths for region: ${region}`,
+	);
+	console.log(staticPaths);
 
 	return staticPaths;
 }
